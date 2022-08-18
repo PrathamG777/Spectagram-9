4629fec96d50c095b68bd97373d393b0f0f7298f
PrathamG777 <81462817+PrathamG777@users.noreply.github.com>
2022-08-18 20:43:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCSVwrxWj14HNXCR3xi52s3yp0ppk5rnxc",
+    authDomain: "spectagram-8d902.firebaseapp.com",
+    projectId: "spectagram-8d902",
+    storageBucket: "spectagram-8d902.appspot.com",
+    messagingSenderId: "79189286255",
+    appId: "1:79189286255:web:3af80ce0169ac98a8f5c9a"
+};
